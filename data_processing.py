e760da4fecfc92d295f138d3ccfca94588a84445
kcajf <4301021+kcajf@users.noreply.github.com>
2026-03-13 10:17:51 +0000

    Update pinned source data

--- data_processing.py
+++ data_processing.py
@@ -24,7 +24,7 @@ log = logging.getLogger(__name__)
 
 # Timestamp version of pinned source data in R2.
 # Update by running: pixi run pin-source-data
-SOURCE_DATA_VERSION = "2026-03-12T192308Z"
+SOURCE_DATA_VERSION = "2026-03-13T100708Z"
 
 
 class CacheManager:
